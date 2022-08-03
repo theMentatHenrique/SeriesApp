e804e552a7f08376740f8e76f7ac8125baf11987
Henrique Paim <henrifeijo13@gmail.com>
2022-08-03 12:08:24 -0300

    imports desnecessarios apagados

--- src/Pages/SeriesPage.js
+++ src/Pages/SeriesPage.js
@@ -1,10 +1,8 @@
-import react from "react";
 import React from "react";
 import {View,Text} from 'react-native';
 
 
 const SeriesPage=({series})=>{
-console.log('chegay aqui');
 return(
 <View>
     <Text>
