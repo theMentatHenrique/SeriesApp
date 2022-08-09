d79f5bfb2d903bc6d11664001617956145c16786
Henrique Paim <henrifeijo13@gmail.com>
2022-08-08 21:35:19 -0300

    teste git lens

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,164 +1,147 @@
-import React from 'react';
-import { View, TextInput, StyleSheet, Button, ActivityIndicator, Text, Alert } from 'react-native';
-import FormRow from '../Components/FormRow';
-import { initializeApp } from 'firebase/app';
-import 'firebase/auth';
+import React from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+  Text,
+  Alert,
+} from "react-native";
+import FormRow from "../Components/FormRow";
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
 //não ESQUECE o estilo do loading
-import { connect } from 'react-redux';
-import { tryLogin } from '../Actions/UserActions';
-
+import { connect } from "react-redux";
+import { tryLogin } from "../Actions/UserActions";
+//git rm
 
 class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isLoading: false,
+      message: "",
+    };
+  }
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAwBjsj1ibR17xSQGKx0y0RvSg68uu7BsQ",
+      authDomain: "series-a1e9e.firebaseapp.com",
+      projectId: "series-a1e9e",
+      storageBucket: "series-a1e9e.appspot.com",
+      messagingSenderId: "397511783524",
+      appId: "1:397511783524:web:f75a5337f4d2dca1d0db71",
+      measurementId: "G-M13BT9DE59",
+    };
 
-        this.state = {
-            email: '',
-            password: '',
-            isLoading: false,
-            message: '',
+    const teste = initializeApp(firebaseConfig);
+  }
+
+  onChangedHandle(field, value) {
+    this.setState({
+      [field]: value,
+    });
+  }
+  renderButton() {
+    if (this.state.isLoading) {
+      return <ActivityIndicator />;
+    }
+    return (
+      <Button
+        style={Styles.button}
+        title="Entrar"
+        onPress={() => this.tryLogin()}
+      />
+    );
+  }
+  renderMessage() {
+    const { message } = this.state;
+
+    if (!message) return null;
+    else {
+      return (
+        <View>
+          <Text>{message}</Text>
+        </View>
+      );
+    }
+  }
+  getMessageByErrorCode(errorCode) {
+    switch (errorCode) {
+      case "auth/wrong-password":
+        return "Senha incorreta";
+
+      case "auth/user-not-found":
+        return "Email incorreto ou inexistente";
+      default:
+        return "Erro desconhecido";
+    }
+  }
+
+  tryLogin() {
+    this.setState({ isLoading: true, message: "" });
+    const { email, password } = this.state;
+    const teste = this.props
+      .tryLogin({ email, password })
+      .then((user) => {
+        if (user) {
+          return this.props.navigation.replace("Main");
         }
-    }
-    componentDidMount() {
-        const firebaseConfig = {
-            apiKey: "AIzaSyAwBjsj1ibR17xSQGKx0y0RvSg68uu7BsQ",
-            authDomain: "series-a1e9e.firebaseapp.com",
-            projectId: "series-a1e9e",
-            storageBucket: "series-a1e9e.appspot.com",
-            messagingSenderId: "397511783524",
-            appId: "1:397511783524:web:f75a5337f4d2dca1d0db71",
-            measurementId: "G-M13BT9DE59"
-        };
-
-        const teste = initializeApp(firebaseConfig);
-
-    }
-
-
-    onChangedHandle(field, value) {
 
         this.setState({
-            [field]: value
+          isLoading: false,
+          message: "",
         });
-
-    }
-    renderButton() {
-
-        if (this.state.isLoading) {
-            return <ActivityIndicator />;
-        }
-        return (
-            <Button style={Styles.button}
-                title="Entrar"
-                onPress={() => this.tryLogin()}
-            />
-        );
-
-    }
-    renderMessage() {
-        const { message } = this.state
-
-        if (!message) return null
-
-        else {
-            return (
-                <View><Text>{message}</Text></View>
-
-            );
-        }
-    }
-    getMessageByErrorCode(errorCode) {
-        switch (errorCode) {
-
-            case 'auth/wrong-password':
-                return 'Senha incorreta';
-
-            case 'auth/user-not-found':
-                return 'Email incorreto ou inexistente';
-            default:
-                return 'Erro desconhecido';
-        }
-
-
-    }
-
-    tryLogin() {
-        this.setState({ isLoading: true, message: '' })
-        const { email, password } = this.state;
-        const teste = this.props.tryLogin({ email, password }).
-            then(user => {
-                if (user) { return this.props.navigation.replace('Main'); }
-
-
-                this.setState({
-                    isLoading: false,
-                    message: ''
-                });
-
-            }).catch((error => {
-                this.setState({
-                    isLoading: false, message:
-                        this.getMessageByErrorCode(error.code)
-                })
-
-
-            }))
-
-
-
-    }
-    render() {
-
-        return (
-            <View>
-                <FormRow first>
-                    <TextInput
-                        style={Styles.input}
-                        placeholder="E-mail"
-                        value={this.state.email}
-                        onChangeText={value => this.onChangedHandle('email', value)}
-                        keyboardType="email-address"
-                        autoCapitalize='none'
-
-                    />
-                </FormRow>
-                <FormRow last>
-                    <TextInput
-                        style={Styles.input}
-                        placeholder="********"
-                        secureTextEntry
-                        value={this.state.password}
-                        onChangeText={value => this.onChangedHandle('password', value)}
-
-
-
-                    />
-                </FormRow>
-                {this.renderButton()}
-                {this.renderMessage()}
-
-
-            </View>
-
-        );
-
-    }
-
-
+      })
+      .catch((error) => {
+        this.setState({
+          isLoading: false,
+          message: this.getMessageByErrorCode(error.code),
+        });
+      });
+  }
+  render() {
+    return (
+      <View>
+        <FormRow first>
+          <TextInput
+            style={Styles.input}
+            placeholder="E-mail"
+            value={this.state.email}
+            onChangeText={(value) => this.onChangedHandle("email", value)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </FormRow>
+        <FormRow last>
+          <TextInput
+            style={Styles.input}
+            placeholder="********"
+            secureTextEntry
+            value={this.state.password}
+            onChangeText={(value) => this.onChangedHandle("password", value)}
+          />
+        </FormRow>
+        {this.renderButton()}
+        {this.renderMessage()}
+      </View>
+    );
+  }
 }
 
 const Styles = StyleSheet.create({
+  input: {
+    paddingRight: 5,
+    paddingLeft: 5,
+    paddingBottom: 5,
+  },
+  button: {
+    backgroundColor: "E50914",
+  },
+});
 
-    input: {
-        paddingRight: 5,
-        paddingLeft: 5,
-        paddingBottom: 5,
-
-    },
-    button: {
-        backgroundColor: 'E50914',
-    },
-})
-
-export default connect(null, { tryLogin })(LoginPage)
+export default connect(null, { tryLogin })(LoginPage);
